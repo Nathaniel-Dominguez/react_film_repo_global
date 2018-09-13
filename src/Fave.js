3ddf4ba3e5012a420af1f32e771aad0c0216fb3b
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-12 18:01:33 -0700

    finished lab 2

---
+++ src/Fave.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Fave extends Component {
+	constructor(props) {
+		super()
+		this.state = {
+			isFave: false
+		}
+	}
+
+	handleClick = (e) => {
+		e.stopPropagation()
+		this.setState({
+			isFave: !this.state.isFave
+		})
+		console.log('handling fave click')
+	} 
+
+	render() {
+		const toQueue = this.state.isFave ? "remove_from_queue" : "add_to_queue"; 
+		return (
+			<div className={`film-row-fave ${toQueue}`} onClick={this.handleClick}>
+  				<p className="material-icons">add_to_queue</p>
+			</div>
+			)
+	}
+}
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import FilmPoster from './FilmPoster';
+import Fave from './Fave';
 
 export default class FilmRow extends Component {
+	handleDetailsClick = (film) => {
+		console.log("Fetching details for", film.title)
+	}
+
 	render() {
-		console.log("FilmRow's this.props.film:",this.props.film);
 		return (
-			<div className="film-row">
+			<div className="film-row" onClick={ () => this.handleDetailsClick(this.props.film) }>
   				<FilmPoster film={this.props.film}/>
 
   				<div className="film-summary">
-   					<h1>TITLE</h1>
-    				<p>YEAR</p>
+   					<h1>{this.props.film.title}</h1>
+    				<p>{this.props.film.release_date.split('-')[0]}</p>
   				</div>
+				<Fave />
 			</div>
 			)
 	}
